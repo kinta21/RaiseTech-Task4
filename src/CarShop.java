a56606bd172bc82732258ec79ed9baa8e17270a0
kinta21 <kinta21127@yahoo.ne.jp>
2023-08-13 09:07:43 +0900

    ファイルの末尾に空行を追加＂ $ git commit --allow-empty -m ファイルの末尾に空行を追加＂

---
+++ src/CarShop.java
@@ -0,0 +1,21 @@
+public class CarShop {
+    private String name;
+    private int founding;
+
+    public CarShop(String name, int founding) {
+        this.name = name;
+        this.founding = founding;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getFounding() {
+        return founding;
+    }
+}
+
+
+
+
